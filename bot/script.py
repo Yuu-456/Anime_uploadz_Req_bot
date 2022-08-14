a44c2ef464fbe00a04aafe70b75b4e0e28bef8ef
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-14 17:30:48 +0530

    Update script.py

--- bot/script.py
+++ bot/script.py
@@ -1,1 +1,17 @@
+PRIVATE_START_MESSAGE = """
+Hey {}!
+Welcome to ☆Anime-UploadzXReqBot☆
 
+• You can request anime as well! Just type #request  {anime name which you wanna request}
+
+.       【V๏ɪ፝֟𝔡】  Network (https://t.me/voidxnetwork)       .
+┗━✦❘༻              ༺❘✦━━┛
+× ᴀɴɪᴍᴇ : @Anime_Uploadz 
+× ᴍᴇᴍᴇꜱ : @Horimiya_Summons 
+× ᴀᴍᴠ : @void_amv
+
+--- ᴘᴏᴡᴇʀᴇᴅ ʙʏ --- 
+@VoidXNetwork
+"""
+
+Photo = "https://telegra.ph/file/f7dc7fde739199db8904f.jpg"
